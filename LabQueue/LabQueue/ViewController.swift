1df4efbc2f7f659d025cae33fa698cbb1e6401c5
Mitch Hamburger <MitchHamburger@Mitchs-MacBook-Pro.local>
2016-06-07 22:16:36 -0400

    initialization

---
+++ LabQueue/LabQueue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LabQueue
+//
+//  Created by Mitch Hamburger on 6/7/16.
+//  Copyright © 2016 Mitch Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
