2a3c614191b74a37b97c61f598b47c02dc05b841
Mitch Hamburger <MitchHamburger@nat-tvwna-inside-visitornet101-b-1115.princeton.org>
2016-06-08 13:45:59 -0400

    updated constraints + actual table view

---
+++ LabQueue/LabQueue/TAHomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TAHomeViewController.swift
+//  LabQueue
+//
+//  Created by Mitch Hamburger on 6/8/16.
+//  Copyright © 2016 Mitch Hamburger. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class TAHomeViewController: UIViewController {
+    
+    @IBOutlet weak var queueTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
