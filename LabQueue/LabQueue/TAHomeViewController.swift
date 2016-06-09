9c77ecb8773ade9b18e2d1134f7375f2d932427f
Mitch Hamburger <MitchHamburger@Mitchs-MacBook-Pro.local>
2016-06-09 16:06:13 -0400

    Successfully showing Queue from web service in TableViews

--- LabQueue/LabQueue/TAHomeViewController.swift
+++ LabQueue/LabQueue/TAHomeViewController.swift
@@ -16,8 +16,11 @@ class TAHomeViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet weak var titleBar: UINavigationBar!
     var items = ["Dog", "Cat", "Cow", "Platypus"]
+    var students = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getQueueData("http://localhost:5000/LabQueue/v1/Queue")
         self.queueTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.queueTable.dataSource = self
         self.queueTable.delegate = self
@@ -26,11 +29,11 @@ class TAHomeViewController: UIViewController, UITableViewDataSource, UITableView
     
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return students.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.queueTable.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
-        cell.textLabel!.text = self.items[indexPath.row]
+        cell.textLabel!.text = self.students[indexPath.row]
         return cell
     }
     
@@ -39,6 +42,48 @@ class TAHomeViewController: UIViewController, UITableViewDataSource, UITableView
         print("you tapped \(indexPath.row)")
     }
 
+    
+    func getQueueData(urlString: String) {
+        let url: NSURL = NSURL(string: urlString)!
+        let session = NSURLSession.sharedSession()
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "GET"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        let semaphore = dispatch_semaphore_create(0)
+        let task = session.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                print("error")
+                return
+            }
+            if error == nil && data != nil {
+                do {
+                    // Convert NSData to Dictionary where keys are of type String, and values are of any type
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
+                    
+                    //let str = json["Queue"]![0]["Name"] as! String
+                    
+                    //print((json["Queue"]! as! NSArray).count)
+                    var count = 0
+                    for student in (json["Queue"]! as! NSArray) {
+                        self.students.append(student["Name"] as! String)
+                        count += 1
+                    }
+                    
+                } catch {
+                    // Something went wrong
+                }
+            }
+            print(self.students)
+            dispatch_semaphore_signal(semaphore)
+        }
+        task.resume()
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+    }
+
+    
     @IBAction func nextStudentPushed(sender: UIButton) {
         let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("StudentViewController")
         self.showViewController(vc as! UIViewController, sender: vc)
