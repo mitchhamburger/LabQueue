5f7cca7212aabcd0a3abe995c1d74d77abe94439
Mitch Hamburger <MitchHamburger@Mitchs-MacBook-Pro.local>
2016-06-09 11:54:13 -0400

    Set up table views for queues

--- LabQueue/LabQueue/TAHomeViewController.swift
+++ LabQueue/LabQueue/TAHomeViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 @IBDesignable
 
-class TAHomeViewController: UIViewController {
+class TAHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     
     @IBOutlet weak var queueTable: UITableView!
     
     @IBOutlet weak var titleBar: UINavigationBar!
-    
+    var items = ["Dog", "Cat", "Cow", "Platypus"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.queueTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.queueTable.dataSource = self
+        self.queueTable.delegate = self
         titleBar.topItem?.title = "Your Next Student is Mitch"
     }
     
+    //UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.queueTable.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel!.text = self.items[indexPath.row]
+        return cell
+    }
+    
+    //UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("you tapped \(indexPath.row)")
+    }
+
     @IBAction func nextStudentPushed(sender: UIButton) {
         let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("StudentViewController")
         self.showViewController(vc as! UIViewController, sender: vc)
