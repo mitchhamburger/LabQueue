3320031be938e18d418ffceffa81d293044fe881
Mitch Hamburger <MitchHamburger@Mitchs-MacBook-Pro.local>
2016-06-09 16:06:13 -0400

    Successfully showing Queue from web service in TableViews

--- LabQueue/LabQueue/StudentHomeViewController.swift
+++ LabQueue/LabQueue/StudentHomeViewController.swift
@@ -14,21 +14,25 @@ class StudentHomeViewController: UIViewController, UITableViewDataSource, UITabl
     
     @IBOutlet weak var queueTable: UITableView!
     var items = ["Dog", "Cat", "Cow", "Platypus"]
+    var students = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getQueueData("http://localhost:5000/LabQueue/v1/Queue")
+        print(students)
         self.queueTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.queueTable.dataSource = self
         self.queueTable.delegate = self
-        getQueueData("http://localhost:5000/LabQueue/v1/Queue")
+        //getQueueData("http://localhost:5000/LabQueue/v1/Queue")
     }
     
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return students.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.queueTable.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
-        cell.textLabel!.text = self.items[indexPath.row]
+        cell.textLabel!.text = self.students[indexPath.row]
         return cell
     }
     
@@ -43,6 +47,8 @@ class StudentHomeViewController: UIViewController, UITableViewDataSource, UITabl
         let request = NSMutableURLRequest(URL: url)
         request.HTTPMethod = "GET"
         request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        let semaphore = dispatch_semaphore_create(0)
         let task = session.dataTaskWithRequest(request) {
             (
             let data, let response, let error) in
@@ -50,13 +56,29 @@ class StudentHomeViewController: UIViewController, UITableViewDataSource, UITabl
                 print("error")
                 return
             }
-            let dataString = NSString(data: data!, encoding:NSUTF8StringEncoding)
-            print(dataString)
+            if error == nil && data != nil {
+                do {
+                    // Convert NSData to Dictionary where keys are of type String, and values are of any type
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
+                    
+                    //let str = json["Queue"]![0]["Name"] as! String
+                    
+                    //print((json["Queue"]! as! NSArray).count)
+                    var count = 0
+                    for student in (json["Queue"]! as! NSArray) {
+                        self.students.append(student["Name"] as! String)
+                        count += 1
+                    }
+                    
+                } catch {
+                    // Something went wrong
+                }
+            }
+            print(self.students)
+            dispatch_semaphore_signal(semaphore)
         }
         task.resume()
-    }
-    
-    func setLabels(queueData: NSData) {
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
     }
 
     @IBAction func addNamePushed(sender: UIButton) {
