202d516fd1a4e6ccd7a564b80cc60d257eb2410b
Mitch Hamburger <MitchHamburger@Mitchs-MacBook-Pro.local>
2016-06-09 11:54:13 -0400

    Set up table views for queues

--- LabQueue/LabQueue/StudentHomeViewController.swift
+++ LabQueue/LabQueue/StudentHomeViewController.swift
@@ -10,15 +10,31 @@
 import UIKit
 @IBDesignable
 
-class StudentHomeViewController: UIViewController {
+class StudentHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var queueTable: UITableView!
-    
+    var items = ["Dog", "Cat", "Cow", "Platypus"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.queueTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.queueTable.dataSource = self
+        self.queueTable.delegate = self
     }
     
+    //UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.queueTable.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel!.text = self.items[indexPath.row]
+        return cell
+    }
     
+    //UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("you tapped \(indexPath.row)")
+    }
     @IBAction func addNamePushed(sender: UIButton) {
         let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("NewFormViewController")
         self.showViewController(vc as! UIViewController, sender: vc)
