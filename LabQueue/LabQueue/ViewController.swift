067870f5404a34304309d90692bc5e3c38fd1a24
Mitch Hamburger <MitchHamburger@nat-oitwireless-inside-vapornet100-b-525.Princeton.EDU>
2016-06-15 11:28:03 -0400

    google authentication

--- LabQueue/LabQueue/ViewController.swift
+++ LabQueue/LabQueue/ViewController.swift
@@ -6,20 +6,148 @@
 //  Copyright © 2016 Mitch Hamburger. All rights reserved.
 //
 
+import GoogleAPIClient
+import GTMOAuth2
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let kKeychainItemName = "Gmail API"
+    private let kClientID = "455520132123-1oq71nkc0mdl007hsiig4acu30h5jg6l.apps.googleusercontent.com"
+    
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLAuthScopeGmailReadonly]
+    
+    private let service = GTLServiceGmail()
+    let output = UITextView()
+    
+    // When the view loads, create necessary subviews
+    // and initialize the Gmail API service
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        output.frame = view.bounds
+        output.editable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        
+        view.addSubview(output);
+        
+        if let auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychainForName(
+            kKeychainItemName,
+            clientID: kClientID,
+            clientSecret: nil) {
+            service.authorizer = auth
+        }
+        
     }
-
+    
+    // When the view appears, ensure that the Gmail API service is authorized
+    // and perform API calls
+    override func viewDidAppear(animated: Bool) {
+        if let authorizer = service.authorizer,
+            canAuth = authorizer.canAuthorize where canAuth {
+            fetchLabels()
+        } else {
+            presentViewController(
+                createAuthController(),
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
+    // Construct a query and get a list of upcoming labels from the gmail API
+    func fetchLabels() {
+        output.text = "Getting labels..."
+        
+        let query = GTLQueryGmail.queryForUsersLabelsList()
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinishSelector: "displayResultWithTicket:finishedWithObject:error:"
+        )
+    }
+    
+    // Display the labels in the UITextView
+    func displayResultWithTicket(ticket : GTLServiceTicket,
+                                 finishedWithObject labelsResponse : GTLGmailListLabelsResponse,
+                                                    error : NSError?) {
+        
+        if let error = error {
+            showAlert("Error", message: error.localizedDescription)
+            return
+        }
+        
+        var labelString = ""
+        self.performSegueWithIdentifier("UserLoggedIn", sender: nil)
+        
+        
+        /*if !labelsResponse.labels.isEmpty {
+            labelString += "Labels:\n"
+            for label in labelsResponse.labels as! [GTLGmailLabel] {
+                labelString += "\(label.name)\n"
+                
+            }
+        } else {
+            labelString = "No labels found."
+        }
+        
+        output.text = labelString*/
+        
+        
+    }
+    
+    
+    // Creates the auth controller for authorizing access to Gmail API
+    private func createAuthController() -> GTMOAuth2ViewControllerTouch {
+        let scopeString = scopes.joinWithSeparator(" ")
+        return GTMOAuth2ViewControllerTouch(
+            scope: scopeString,
+            clientID: kClientID,
+            clientSecret: nil,
+            keychainItemName: kKeychainItemName,
+            delegate: self,
+            finishedSelector: "viewController:finishedWithAuth:error:"
+        )
+    }
+    
+    // Handle completion of the authorization process, and update the Gmail API
+    // with the new credentials.
+    func viewController(vc : UIViewController,
+                        finishedWithAuth authResult : GTMOAuth2Authentication, error : NSError?) {
+        
+        if let error = error {
+            service.authorizer = nil
+            showAlert("Authentication Error", message: error.localizedDescription)
+            return
+        }
+        
+        service.authorizer = authResult
+        print(authResult.userEmail)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.Default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
